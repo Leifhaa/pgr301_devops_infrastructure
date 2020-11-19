dc0d73481d8ada72513679ac7bd825bc76ab4915
Leifhaa <leifhaa@hotmail.com>
2020-11-19 23:34:04 +0100

    Added version in requiredproviders

--- provider.tf
+++ provider.tf
@@ -3,6 +3,7 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
+      version = "~> 3.48.0"
     }
   }
 }
@@ -20,7 +21,6 @@ terraform {
 #A privder is used to configure the named provider. A provider is responsible for creating resources
 #multiple provider blocks can exist if Terraform configuration manages resources from different providers
 provider "google" {
-  version = "3.5.0"
   credentials = file("terraform_keyfile.json")
   project     = "exam-295817"
   region      = "europe-north1-a"
