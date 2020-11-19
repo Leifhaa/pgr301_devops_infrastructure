a9d089f2760c1b14cb3064357cf4438dbec71277
Leifhaa <leifhaa@hotmail.com>
2020-11-20 00:00:34 +0100

    Changed to google, not beta

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+terraform {
   #Save the terraform state (backend) to google cloud storage (gcs)
   backend "gcs" {
     bucket = "exam-295817-tf-state"
@@ -10,9 +18,10 @@ terraform {
 
 #A privder is used to configure the named provider. A provider is responsible for creating resources
 #multiple provider blocks can exist if Terraform configuration manages resources from different providers
-provider "google-beta" {
+provider "google" {
+  version = "3.5.0"
   credentials = file("terraform_keyfile.json")
   project     = "exam-295817"
-  region      = "europe-north1-a"
-  version = "~> 3.0.0-beta.1"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
