d312b1390c2291cf0bf4a2cac4244a14a5b3f8c2
Leifhaa <leifhaa@hotmail.com>
2021-02-24 15:03:24 +0100

    Changed from EC2 service to dynamoDB.

--- aws.tf
+++ aws.tf
@@ -1,4 +1,12 @@
-resource "aws_instance" "example" {
-  ami = "ami-2757f631"
-  instance_type = "t2.micro"
+resource "aws_dynamodb_table" "billing-table" {
+  hash_key = "UserId"
+  name = "Billing"
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+  attribute {
+    name = "Amount"
+    type = "N"
+  }
 }
